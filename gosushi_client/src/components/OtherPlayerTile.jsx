27590a2dd3e56a565bff8ef100f1d1c8b0956348
Hannah Li <hannahli2010@gmail.com>
2020-08-21 17:40:29 -0400

    Tile for other player information

---
+++ gosushi_client/src/components/OtherPlayerTile.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+import CardToggle from './CardToggle';
+import { getCardImage } from '../utils/getCardImage';
+import './otherPlayerTile.scss';
+
+const OtherPlayerTile = ({ player }) => {
+  const [showPlayedCards, toggleShowPlayedCards] = useState(true);
+
+  const displayPlayedCard = (card, index) => (
+    <div key={`played_${card.name}_${index}`}>
+      <img
+        className="card-image-other-players"
+        src={getCardImage(card)}
+        alt={card.name}
+      />
+    </div>
+  );
+
+  const cardsToShow = showPlayedCards
+    ? player.playedCards
+    : player.dessertCards;
+
+  return (
+    <div className="container-player-data">
+      <div className="points-circle">
+        <span>{player.points}</span>
+      </div>
+      <div className="toggle-row">
+        <CardToggle
+          checked={showPlayedCards}
+          onClick={() => toggleShowPlayedCards(!showPlayedCards)}
+        />
+      </div>
+      <div className="container-cards">
+        {cardsToShow.map(displayPlayedCard)}
+      </div>
+      <span>{player.name}</span>
+    </div>
+  );
+};
+
+export default OtherPlayerTile;
