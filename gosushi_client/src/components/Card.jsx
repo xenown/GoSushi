044e77a4fdefecd1031b55ef63e5021e38772321
Frank <frankwu26@gmail.com>
2020-08-27 16:33:08 -0400

    Consolidate card animation

--- gosushi_client/src/components/Card.jsx
+++ gosushi_client/src/components/Card.jsx
@@ -3,18 +3,20 @@ import { useSpring, useChain, animated, config } from 'react-spring';
 import { getCardImage } from '../utils/getCardImage';
 
 const Card = ({ className, card, index, isSelected, handleSelectCard, startWidth, startHeight, scaleUpFactor, transform, imageClass}) => {
-  const [isHovering, setHover] = useState(false);
+  const [hoverTimer, setHoverTimer] = useState(null);
+  const [isHover, setHover] = useState(false);
 
   const posRef = useRef();
   const posProps= useSpring({
     ref: posRef,
-    to: isHovering ? { zIndex: 1 } : { zIndex: 0 },
+    to: isHover ? { zIndex: 1 } : { zIndex: 0 },
     immediate: true
   });
 
   const transformHover = transform && transform.hover && transform.noHover ? transform.hover : "scale(2) translateY(-25%)";
   const transformNoHover = transform && transform.hover && transform.noHover ? transform.noHover : "scale(1) translateY(0%)";
-  const transformSelected = transform && transform.selected ? transform.selected : "scale(1) translateY(-10%)";
+  const transformHoverSelected = transform && transform.hoverSelected ? transform.hoverSelected : "scale(2) translateY(-35%)";
+  const transformNoHoverSelected = transform && transform.noHoverSelected ? transform.noHoverSelected : "scale(1) translateY(-10%)";
   const width = startWidth ? startWidth : null;
   const height = startHeight ? startHeight : null;
   const scaleDirectHover = height ? { height: `${height * scaleUpFactor}vh` } : width ? { width: `${width * scaleUpFactor}vw` } : {};
@@ -23,10 +25,8 @@ const Card = ({ className, card, index, isSelected, handleSelectCard, startWidth
   const sizeRef = useRef();
   const sizeProps = useSpring({
     ref: sizeRef,
-    to: isHovering ? { ...scaleDirectHover, transform: `${transformHover}` }
-                   : { ...scaleDirectNoHover, transform: `${isSelected && transformSelected ? transformSelected : transformNoHover}` },
-    config: config.default,
-    delay: isHovering ? 400 : 0
+    to: { transform: isHover ? isSelected ? transformHoverSelected : transformHover : isSelected ? transformNoHoverSelected : transformNoHover },
+    config: config.default
   });
 
   useChain([posRef, sizeRef]);
@@ -36,8 +36,8 @@ const Card = ({ className, card, index, isSelected, handleSelectCard, startWidth
   return (
       <animated.div
         className={finalClassName}
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
+        onMouseEnter={() => setHoverTimer(setTimeout(() => { setHover(true); }, 1000))}
+        onMouseLeave={() => { clearTimeout(hoverTimer); setHover(false); }}
         style={{...posProps, ...sizeProps, position: "relative"}}
         key={index}
         onClick={() => onClick(index)}
