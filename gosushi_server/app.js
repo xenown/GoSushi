0b72d485804b1b4b35d9cb089cd6cfd57aff97e1
Hannah Li <hannahli2010@gmail.com>
2020-09-27 19:35:23 -0400

    Additional file changes for heroku deployment

--- gosushi_server/app.js
+++ gosushi_server/app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 
-const port = process.env.SERVERPORT;
+const port = process.env.PORT || 5000;
 const index = require('./routes/index');
+const path = require('path');
 
 const app = express();
 // const cors = require('cors');
@@ -11,6 +12,11 @@ const app = express();
 app.set('port', port);
 // app.use(cors());
 app.use(index);
+
+app.use(express.static(path.resolve(__dirname, '../gosushi_client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../gosushi_client/build', 'index.html'));
+})
 
 const server = http.createServer(app);
 const io = socketIo(server);
--- gosushi_server/routes/index.js
+++ gosushi_server/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/test', (req, res) => {
   res.send({ response: 'I am alive' }).status(200);
 });
 
